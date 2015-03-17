8e21051a00d2aa05fd3df7af2b4d0987f4631fec
Corbzilla <jason.ron.corbett@gmail.com>
2015-03-17 14:31:08 +0000

    initial documentation commit

--- src/Kronicle.js
+++ src/Kronicle.js
@@ -2,41 +2,67 @@ import {Core} from './Core.js';
 import {events} from './EventTypes.js';
 import {default as PubSub} from 'pubsub-js';
 
+// # Main Kronicle Class
+// depends: Kronicle.Core, Kronicle.EventTypes, pubsub-js
+// The main Kronicle Class, constructor takes no arguments, initializes the core with a null value.
+// Use build method to setup a core with an array of Kronicle modules passed as an argument.
 export class Kronicle {
     constructor(){
         this.core = null;
     }
     
+    // ## build method
+    // Takes an array f Kronicle modules as an argument and initializes the core.Returns a reference to the object.
     build (args = { modules: [] }) {
         this.core = new Core(args.modules);
         return this;
     }
     
-    initialize(cb) {
+
+    // ## initialize method
+    // Takes a callback as an argument.
+    // Callback is called when Kronicle Core is initialized.    initialize(cb) {
         PubSub.subscribe(events.Initialized, cb);
         return this;
     }
     
+
+    // ## beforeModulesLoad method
+    // Takes a callback as an argument.
+    // Callback is called before any Kronicle modules are loaded, callback receives all modules as an argument ie cb(modules).
     beforeModulesLoad(cb)  {
         PubSub.subscribe(events.BeforeModulesLoad, cb);
         return this;
     }
     
+    // ## moduleLoaded method
+    // Takes a callback as an argument.
+    // Callback is called as each module is loaded, callback receives module as an argument ie cb(module)
     moduleLoaded(cb) {
         PubSub.subscribe(events.ModuleLoaded, cb);
         return this;
     }
     
+    // ## afterModulesLoad method
+    // Takes a callback as an argument.
+    // Callback is called after all modules are loaded, callback receives all modules as argument ie cb(modules)
     afterModulesLoad(cb){
         PubSub.subscribe(events.AfterModulesLoad, cb);
         return this;
     }
     
+    // ## ready method
+    // Takes a callback as an argument.
+    // Callback is called after all modules are loaded an Kronicle is ready for use.
     ready(cb) {
         PubSub.subscribe(events.Ready, cb);
         return this;
     }
     
+    // ## start method
+    // Takes a callback as an argument.
+    // Callback is called when Kronicle is ready to be used, callback receives the core with added modules as a callback.
+    // Should be used as main entry point for kronicle apps.
     start(cb){
         cb.call(this, this.core.addedModules);
         PubSub.publish(events.Start);
